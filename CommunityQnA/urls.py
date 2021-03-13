ef4c00573649016430a6a6a6a2f068333bd28e88
SalinaK1 <saleena.koirala1@gmail.com>
2021-03-14 00:50:56 +0545

    Community QnA working.

--- CommunityQnA/urls.py
+++ CommunityQnA/urls.py
@@ -6,9 +6,13 @@
 # ]
 
 from rest_framework import routers
-from .views import QnAAPI
+from .views import QnAAPI, qna_get, qna_view
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register('qna', QnAAPI, 'qna')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('qna_list/', qna_get),
+    path('qna/', qna_view)
+]
--- CommunityQnA/views.py
+++ CommunityQnA/views.py
@@ -7,6 +7,9 @@
 from .models import QnA
 from .serializers import QnASerializer
 from rest_framework import viewsets, mixins
+from rest_framework.generics import CreateAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 class QnAAPI(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
@@ -14,3 +17,16 @@ class QnAAPI(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveMo
     serializer_class = QnASerializer
 
 
+@api_view(['GET'])
+def qna_get(request, *args, **kwargs):
+    qna = QnA.objects.all()
+    serializer = QnASerializer (qna, many=True)
+    return Response(serializer.data, status = 200)
+
+@api_view(['POST'])
+def qna_view(request, *args, **kwargs):
+    serializer = QnASerializer(data = request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
